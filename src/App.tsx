6997e115ec6c1bfb750dfeb098a452cd66c44546
caducasara <carloscasara98@gmail.com>
2023-04-07 15:29:54 -0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
